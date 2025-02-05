397af3530dad4dc81de092b29ec7a4378fdda021
M Sufyan Shaikh <sufyan.souloftware@gmail.com>
2025-02-05 17:16:49 +0500

    initial files

---
+++ newsletter-plugin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Newsletter Subscription
+ * plugin URI: https://souloftware.com/
+ * Description: A simple newsletter subscription plugin with AJAX and database storage.
+ * Version: 1.0
+ * Author: Souloftware
+ * Author URI: https://souloftware.com/contact
+ */
+
+// Exit if accessed directly
+if (!defined('ABSPATH'))
+    exit;
+
+// Define plugin path
+define('NEWSLETTER_PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+// Include necessary files
+require_once NEWSLETTER_PLUGIN_DIR . 'includes/database.php';
+require_once NEWSLETTER_PLUGIN_DIR . 'includes/shortcode.php';
+require_once NEWSLETTER_PLUGIN_DIR . 'includes/ajax-handler.php';
+require_once NEWSLETTER_PLUGIN_DIR . 'includes/admin-page.php';
+
+// Activation Hook - Create Database Table
+register_activation_hook(__FILE__, 'newsletter_create_table');
